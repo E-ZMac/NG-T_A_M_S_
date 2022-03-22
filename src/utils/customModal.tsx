f7fb2b05347b2c44b67b7c839ab1b03a5cd35665
ElijahMac <kilobolt1@mail.com>
2022-03-22 00:03:42 -1000

    making new branch for state hook

---
+++ src/utils/customModal.tsx
@@ -0,0 +1,67 @@
+import React, { memo, useState } from 'react';
+import { 
+    Box,
+    Button,
+    Typography,
+    Modal,
+} from '../lib/ui';
+
+function stateShareMemo(props) {
+
+}
+
+function hasStateChanged(prevProps, nextProps) {
+
+}
+
+export default React.memo(stateShareMemo, hasStateChanged)
+
+export const ModalTemplate = () => {
+  const [open, setOpen] = useState<boolean>(false)
+  const [buttonText, setButtonText] = useState<string>('')
+  const [titleText, setTitleText] = useState<string>('')
+  const [descriptionText, setDescriptionText] = useState<string>('')
+
+  const handleOpenState = () => {
+      if(open === false)
+      return setOpen(true)
+
+      if(open === true)
+      return setOpen(false)
+    }
+
+  return (
+    <div>
+      <Button
+        className={'openButton'}
+        onClick={handleOpenState}
+        >
+        {buttonText}
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleOpenState}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box>
+          <Typography 
+            className={'titleText'}
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+            >
+            {titleText}
+          </Typography>
+          <Typography
+            className={'descriptionText'}
+            id="modal-modal-description"
+            sx={{ mt: 2 }}
+            >
+            {descriptionText}
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
